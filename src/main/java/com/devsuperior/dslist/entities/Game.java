3ebab675cd9b21b6e2aba78fe06088ede3290227
Denison Resplandes <denison.si2014@gmail.com>
2023-07-11 22:46:56 -0300

    criar projeto, entidade game e perfil de teste

---
+++ src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -0,0 +1,49 @@
+package com.devsuperior.dslist.entities;
+
+import java.io.Serializable;
+import java.time.Year;
+
+import org.hibernate.annotations.DynamicUpdate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "game")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@DynamicUpdate
+public class Game implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@EqualsAndHashCode.Include
+	private Long id;
+	private String title;
+	
+	@Column(name = "_year")
+	private Year year;
+	private String genre;
+	private String platforms;
+	private Double score;
+	private String imgUrl;
+	
+	@Column(columnDefinition = "TEXT")
+	private String shortDescription;
+	@Column(columnDefinition = "TEXT")
+	private String longDescription;
+}
