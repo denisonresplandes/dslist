b0f4bfedf9e89b6a7f998b1f2dfec6720785393b
Denison Resplandes <denison.si2014@gmail.com>
2023-07-27 20:43:59 -0300

    Implementar movimentação dos games nas Listas

--- src/main/java/com/devsuperior/dslist/controllers/GameListController.java
+++ src/main/java/com/devsuperior/dslist/controllers/GameListController.java
@@ -7,11 +7,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.dslist.dto.GameListDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
+import com.devsuperior.dslist.dto.ReplacementDTO;
 import com.devsuperior.dslist.services.GameListService;
 import com.devsuperior.dslist.services.GameService;
 
@@ -38,5 +41,11 @@ public class GameListController {
 	@GetMapping("/{listId}/games")
 	public ResponseEntity<List<GameMinDTO>> findByList(@PathVariable Long listId) {
 		return ResponseEntity.status(HttpStatus.FOUND).body(gameService.findByList(listId));
+	}
+	
+	@PostMapping("/{listId}/replacement")
+	public ResponseEntity<Void> move(@PathVariable Long listId, @RequestBody ReplacementDTO body) {
+		service.move(listId, body.getSourceIndex(), body.getDestinationIndex());
+		return ResponseEntity.noContent().build();
 	}
 }
--- src/main/java/com/devsuperior/dslist/services/GameListService.java
+++ src/main/java/com/devsuperior/dslist/services/GameListService.java
@@ -1,5 +1,6 @@
 package com.devsuperior.dslist.services;
 
+import java.util.LinkedList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,13 +10,18 @@ import org.springframework.transaction.annotation.Transactional;
 import com.devsuperior.dslist.dto.GameListDTO;
 import com.devsuperior.dslist.entities.GameList;
 import com.devsuperior.dslist.exceptions.ResourceNotFoundException;
+import com.devsuperior.dslist.projections.GameMinProjection;
 import com.devsuperior.dslist.repositories.GameListRepository;
+import com.devsuperior.dslist.repositories.GameRepository;
 
 @Service
 public class GameListService {
 
 	@Autowired
 	private GameListRepository repository;
+	
+	@Autowired
+	private GameRepository gameRepository;
 	
 	@Transactional(readOnly = true)
 	public List<GameListDTO> findAll() {
@@ -29,5 +35,18 @@ public class GameListService {
 		GameList gameList = repository.findById(id)
 				.orElseThrow(() -> new ResourceNotFoundException("Game List not found!"));
 		return new GameListDTO(gameList);
+	}
+	
+	@Transactional
+	public void move(Long listId, int sourceIndex, int destinationIndex) {
+		List<GameMinProjection> list = new LinkedList<>(gameRepository.searchByList(listId));
+		GameMinProjection gameProjection = list.remove(sourceIndex);
+		list.add(destinationIndex, gameProjection);
+		int min = Math.min(sourceIndex, destinationIndex);
+		int max = Math.max(sourceIndex, destinationIndex);
+		
+		for (int index = min; index <= max; index++) {
+			repository.updateBelongingPosition(listId, list.get(index).getId(), index);
+		}
 	}
 }
